5cd8bb56d9de57b9477d7054cdc3e0a8dda1574a
AliF50 <alihassan@post.com>
2016-12-23 07:58:38 -0500

    Done

--- app.js
+++ app.js
@@ -1,10 +1,41 @@
-(function () {
-    var app = angular.module("NarrowItDownApp", []);
+(function () { //IEFE
+
+    var app = angular.module("NarrowItDownApp", []); //Declare module
+
+    app.directive("foundItemsInList", function () { //Declare directive
+        var ddo = {
+            templateUrl: 'list.html',
+            scope: {
+                found: '<',
+                onRemove: '&'
+            }
+        };
+
+        return ddo;
+    });
 
     app.controller('NarrowItDownController', ['$scope', 'MenuSearchService', function ($scope, MenuSearchService) {
+        var narrow = this;
 
-        this.NarrowItDown = function (search) {
-            this.results = MenuSearchService.getMatchedMenuItems(search);
+        narrow.search = "";
+
+        narrow.NarrowItDown = function (search) {
+            if (narrow.search === "") {
+                narrow.found = [];
+                return;
+            }
+            var promise = MenuSearchService.getMatchedMenuItems(narrow.search);
+            promise.then(function (response) {
+                    narrow.found = response;
+                })
+                .catch(function (error) {
+                    console.log("Something went wrong", error);
+                });
+        };
+
+
+        narrow.remove = function (index) {
+            this.found.splice(index, 1);
         };
 
     }]);
@@ -15,20 +46,19 @@
             return $http.get('https://davids-restaurant.herokuapp.com/menu_items.json').then(function (response) {
                 menuData = response.data;
 
-                console.log(menuData.menu_items.length);
+                // console.log(menuData.menu_items);
                 var foundItems = [];
                 var lowerCase;
                 for (var i = 0; i < menuData.menu_items.length; i++) {
-                    lowerCase = menuData.menu_items[i].name.toLowerCase();
-                    if (lowerCase.indexOf(term.toLowerCase()) > 0) {
+                    lowerCase = menuData.menu_items[i].description.toLowerCase();
+                    if (lowerCase.indexOf(term.toLowerCase()) >= 0) {
                         foundItems.push(menuData.menu_items[i]);
                     }
 
                 }
-                // console.log(foundItems);
+                //console.log(foundItems.length);
                 return foundItems;
             });
-
         };
                 }]);
 
